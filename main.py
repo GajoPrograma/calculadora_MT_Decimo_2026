e92d0cd4301c0ffdd6e7c2c98a7bc2fcd8a14178
Jose Saavedra <ingelecjosesaavedra@gmail.com>
2026-08-11 15:07:02 -0500

    Add main.py with calculator logic and operation flow

---
+++ main.py
@@ -0,0 +1,52 @@
+import operaciones
+import interfaz
+
+interfaz.limpiar_terminal()
+memoria = []
+num1 = int(input(" "))
+interfaz.limpiar_terminal()
+memoria.append(num1)
+i = "si"
+resultado = 0
+while (i == "si"):
+    interfaz.operacion_anterior(memoria)
+    operacion = input(" ")
+    print(operacion)
+    memoria.append(operacion)
+    interfaz.limpiar_terminal()
+    if operacion == "=":
+        break
+    interfaz.operacion_anterior(memoria)
+    num2 = int(input(" "))
+    interfaz.limpiar_terminal()
+    if operacion == "+":
+        resultado =operaciones.sumar(num1,num2)
+    elif operacion == "-":
+        resultado =operaciones.restar(num1,num2)
+    elif operacion == "*":
+        resultado =operaciones.multiplicar(num1,num2)
+    elif operacion == "^":
+            resultado =operaciones.radicacion(num1,num2)
+    elif operacion == "!":
+            resultado =operaciones.potenciacion(num1,num2)
+    elif operacion == "%":
+            resultado =operaciones.porcentaje(num1,num2)
+    elif operacion == "Log":
+            resultado =operaciones.logaritmacion(num1,num2)
+    elif operacion == "/":
+        if num2 == 0:
+            print("opcion invalida")
+        else:
+            resultado =operaciones.dividir(num1,num2)
+    else:
+        print("opción no valida")
+      
+    
+    memoria.append(num2)
+    num1=resultado
+       
+for i in memoria:
+    
+    print(i, end="")
+
+print("=", resultado)
